016d036a3f6615f1262326b9d127b0e0ae0fe11e
Documenter.jl <documenter@juliadocs.github.io>
2021-10-26 00:58:05 +0000

    build based on c6fbf87

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageBXW","category":"page"},{"location":"#DemoPackageBXW","page":"Home","title":"DemoPackageBXW","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageBXW.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageBXW]","category":"page"}]
+}
