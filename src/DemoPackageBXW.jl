f8ff70c97604ed4151985f85a2a8ce814be85536
binxu.wang <binxu.wang@wustl.edu>
2021-10-25 21:48:58 -0400

    add io generality

--- src/DemoPackageBXW.jl
+++ src/DemoPackageBXW.jl
@@ -1,5 +1,5 @@
 module DemoPackageBXW
-print_greeting() = print("Hello, world!")
+# print_greeting() = print("Hello, world!")
 # Write your package code here.
-
+print_greeting(io::IO = stdout) = print(io, "Hello, world!")
 end
